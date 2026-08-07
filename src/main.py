f7cc65dad4aafcc54fea8d8a0a49d49e1e5c1e5c
Ali Abbaspour <aliabbaspourzx9090@gmail.com>
2026-08-07 12:59:04 +0330

    Update utils and mian.py

--- src/main.py
+++ src/main.py
@@ -4,10 +4,7 @@ from utils import add
 
 print('hello world')
 def main():
-    a = 10
-    b = 5
-    result = add(a, b)
-    print(f"The sum of {a} and {b} is: {result}")
+    return 'hi'
 
 if __name__ == "__main__":
     main()
