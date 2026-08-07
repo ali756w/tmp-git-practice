942e3fa98069636c5f5452dd02bd255260981c13
Ali Abbaspour <aliabbaspourzx9090@gmail.com>
2026-08-07 11:36:47 +0330

    Add docstring to function

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,23 @@
 def add(a, b):
+    """
+    this function adds two numbers
+    """
     return a + b
 
 def sub(a, b):
+    """
+    this function subtracts two numbers
+    """
     return a - b
 
 def mul(a, b):
+    """
+    this function multiplies two numbers
+    """
     return a * b
 
 def div(a, b):
+    """
+    this function divides two numbers
+    """
     return a / b
